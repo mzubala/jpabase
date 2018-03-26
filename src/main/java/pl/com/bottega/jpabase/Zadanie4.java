86b1cf95756d18f265f8bec6f7cca3746113e987
Maciej Zubala <maciej.zubala@gmail.com>
2018-03-26 15:09:58 +0200

    zad4

---
+++ src/main/java/pl/com/bottega/jpabase/Zadanie4.java
@@ -0,0 +1,24 @@
+package pl.com.bottega.jpabase;
+
+public class Zadanie4 extends TaskTemplate {
+  @Override
+  protected void doTask() {
+    Auction auction = new Auction("test");
+    auction.duration = 7;
+    auction.id = 1L;
+    auction.description = "opis";
+    executeInTx((em) -> {
+      em.merge(auction);
+    });
+
+    executeInTx((em) -> {
+      Auction merged = em.merge(auction);
+      merged.title = "nowy nowy";
+    });
+  }
+
+  public static void main(String[] args) {
+    new Zadanie4().execute();
+  }
+
+}
