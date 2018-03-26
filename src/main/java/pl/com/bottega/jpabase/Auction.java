a80f8e0e7fa8a5157efaea01fb25a9f6e341306f
Maciej Zubala <maciej.zubala@gmail.com>
2018-03-26 12:24:12 +0200

    zad1

---
+++ src/main/java/pl/com/bottega/jpabase/Auction.java
@@ -0,0 +1,23 @@
+package pl.com.bottega.jpabase;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Entity
+public class Auction {
+
+  @Id
+  public Long id;
+
+  public String title;
+
+  @Column(length = 6000)
+  public String description;
+
+  public LocalDateTime start;
+
+  public Integer duration;
+
+}
--- src/main/java/pl/com/bottega/jpabase/Zadanie1.java
+++ src/main/java/pl/com/bottega/jpabase/Zadanie1.java
@@ -1,7 +1,21 @@
 package pl.com.bottega.jpabase;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class Zadanie1 {
   public static void main(String[] args) {
+    EntityManagerFactory emf = null;
+    try {
+      emf = Persistence.createEntityManagerFactory("DMS");
+      EntityManager em = emf.createEntityManager();
 
+    }
+    finally {
+      if(emf != null) {
+        emf.close();
+      }
+    }
   }
 }
