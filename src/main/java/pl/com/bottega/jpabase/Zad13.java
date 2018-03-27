fc208f8a2df3f7e1be1e3e78d214d76c376924ca
Maciej Zubala <maciej.zubala@gmail.com>
2018-03-27 16:33:06 +0200

    zad13

---
+++ src/main/java/pl/com/bottega/jpabase/Zad13.java
@@ -0,0 +1,47 @@
+package pl.com.bottega.jpabase;
+
+import antlr.StringUtils;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Optional;
+
+public class Zad13 extends TaskTemplate {
+
+  public static void main(String[] args) {
+    new Zad13().execute();
+  }
+
+  @Override
+  protected void doTask() {
+    executeInTx((em) -> {
+      searchAuctions(em, Optional.of("new"), Optional.of(1L));
+      searchAuctions(em, Optional.empty(), Optional.empty());
+      searchAuctions(em, Optional.of("new"), Optional.empty());
+      searchAuctions(em, Optional.empty(), Optional.of(1L));
+    });
+  }
+
+  private List<Auction> searchAuctions(EntityManager em, Optional<String> query, Optional<Long> userId) {
+    List<String> jpqlComponents = new LinkedList<>();
+    jpqlComponents.add("SELECT DISTINCT(a) FROM Auction a");
+    List<String> where = new LinkedList<>();
+    query.ifPresent((q) -> where.add("(a.title LIKE :like OR a.description LIKE :like)"));
+    userId.ifPresent((id) -> {
+      jpqlComponents.add("JOIN a.bids b JOIN b.user u ");
+      where.add("u.id = :userId");
+    });
+    if (where.size() > 0) {
+      jpqlComponents.add("WHERE");
+      jpqlComponents.add(String.join(" AND ", where));
+    }
+    Query q = em.createQuery(String.join(" ", jpqlComponents));
+    query.ifPresent((phrase) -> q.setParameter("like", "%" + phrase + "%"));
+    userId.ifPresent((id) -> q.setParameter("userId", id));
+    return q.getResultList();
+  }
+
+
+}
