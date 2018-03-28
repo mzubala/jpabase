635cbbef93d810c88366016705579e44098a7508
Maciej Zubala <maciej.zubala@gmail.com>
2018-03-28 12:19:40 +0200

    optimistic locking

--- src/main/java/pl/com/bottega/jpabase/BaseEntity.java
+++ src/main/java/pl/com/bottega/jpabase/BaseEntity.java
@@ -3,12 +3,16 @@ package pl.com.bottega.jpabase;
 import javax.persistence.MappedSuperclass;
 import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
+import javax.persistence.Version;
 import java.time.LocalDateTime;
 
 @MappedSuperclass
 public class BaseEntity {
   public LocalDateTime createdAt;
   public LocalDateTime updatedAt;
+
+  @Version
+  private Long version;
 
   @PrePersist
   void setCreatedAt() {
